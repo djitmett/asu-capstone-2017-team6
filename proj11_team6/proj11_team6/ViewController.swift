92df88b5727fde8d5e2bcfc588ad0b491641bf2d
Darya Jitmetta <djitmett@asu.edu>
2017-10-18 19:05:49 -0700

    Initial template

---
+++ proj11_team6/proj11_team6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  proj11_team6
+//
+//  Created by Darya T Jitmetta on 10/18/17.
+//  Copyright © 2017 Darya T Jitmetta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
