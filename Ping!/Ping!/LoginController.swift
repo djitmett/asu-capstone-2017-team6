3746ee76e9322138b7d920ab4e5f952775a44072
Darya Jitmetta <djitmett@asu.edu>
2017-11-16 13:24:34 -0800

    New login page - Page for login - Currently not connected with DB - Needs full implementation

---
+++ Ping!/Ping!/LoginController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginController.swift
+//  Ping!
+//
+//  Created by Darya T Jitmetta on 11/16/17.
+//  Copyright © 2017 Darya T Jitmetta. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    //MARK: Properties
+    
+    @IBOutlet weak var signupButton: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+
+
+}
