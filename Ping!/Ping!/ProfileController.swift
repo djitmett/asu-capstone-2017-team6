e3e0fa4f76d0c7c3fbc2e0e7ef24176b918211a1
Darya Jitmetta <djitmett@asu.edu>
2017-11-20 13:30:55 -0800

    Setting up pages in storyboard - Set up login page - Set up profile page - Setting up segues

---
+++ Ping!/Ping!/ProfileController.swift
@@ -0,0 +1,38 @@
+//
+//  ProfileController.swift
+//  Ping!
+//
+//  Created by Darya T Jitmetta on 11/20/17.
+//  Copyright © 2017 Darya T Jitmetta. All rights reserved.
+//
+
+import UIKit
+
+class ProfileController: UIViewController {
+
+    @IBAction func logout(_ sender: Any) {
+         performSegue(withIdentifier: "unwindSegueToLogin", sender: self)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
