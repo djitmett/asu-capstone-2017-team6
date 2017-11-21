c2bbaa1594fa40662cf4826b91fec0d6bf558100
Darya Jitmetta <djitmett@asu.edu>
2017-11-21 14:47:08 -0800

    OneSignal Features - Changed AppDelegate to include OneSignal push notif. handling - Added template for tracking invitation - Submit button is only a template so right now for testing it will send your device your phone number upon opening.

--- Ping!/Ping!/TrkRequestViewController.swift
+++ Ping!/Ping!/TrkRequestViewController.swift
@@ -10,35 +10,64 @@ import UIKit
 import OneSignal
 
 class TrkRequestViewController: UIViewController {
-    var player_id = ""
     
+    /**
+     - Text field needs to be implemented & include the 'hide away keyboard'
+     - Grab text field input and store it into a request number variable
+     - Run requested number against the DB
+     - Return a message if it doesn't exist
+     - if it does, proceed by grabbing their assigned player_id and store their phonenumber
+     
+     **/
+    
+    @IBAction func sendTracking(_ sender: Any) {
+        
+        //USER DEFAULTS FOR ONESIGNAL ID
+        let defaults = UserDefaults.standard
+        //CURRENTLY GRABS YOUR INFORMATION TO SEND THE PUSH NOTIFICATION TO YOU
+        let player_id = (defaults.object(forKey: "GT_PLAYER_ID_LAST") as? String)!
+        let userFirstName = (defaults.object(forKey: "userFirstName") as? String)!
+        let phone_number = (defaults.object(forKey: "userPhone") as? String)!
+        
+        //All OneSignal content is in JSON format
+        let data = [
+            "Phone" : phone_number,
+            ]
+        let message = userFirstName + " would like to share their location with you."
+        let notificationContent = [
+            "include_player_ids": [player_id],
+            "contents": ["en": message], // Required unless "content_available": true or "template_id" is set
+            "headings": ["en": "PING! TRACKING"],
+            "subtitle": ["en": "Someone has sent you a tracking invitation."],
+            "data": data,
+            //Displays image in push notif.
+            //"ios_attachments": ["id" : "https://cdn.pixabay.com/photo/2017/01/16/15/17/hot-air-balloons-1984308_1280.jpg"],
+            //Shows notification bage on application
+            "ios_badgeType": "Increase",
+            "ios_badgeCount": 1
+            ] as [String : Any]
+        
+        //Send request and receive confirmation
+        OneSignal.postNotification(notificationContent, onSuccess: { result in
+            print("result = \(result!)")
+        }, onFailure: {error in
+            print("error = \(error!)")
+        })
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //USER DEFAULTS FOR ONESIGNAL ID
-        let defaults = UserDefaults.standard
-        player_id = (defaults.object(forKey: "GT_PLAYER_ID_LAST") as? String)!
         
-        //test notification
-        OneSignal.postNotification(["contents": ["en": "Test Message"], "include_player_ids": [player_id]])
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
 }
