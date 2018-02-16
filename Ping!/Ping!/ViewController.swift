59bc839f30576ddd531df37323f0c68edd66658b
Corey Rakes <crakes@Coreys-MacBook-Air.local>
2018-02-16 15:34:34 -0500

    Change annotation image size

--- Ping!/Ping!/ViewController.swift
+++ Ping!/Ping!/ViewController.swift
@@ -388,11 +388,41 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         dateFormatter.timeStyle = .medium
         return dateFormatter.string(from: dt!)
     }
+    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+        let size = image.size
+        
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+        
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+        }
+        
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+        
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+    }
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         if let annotation = annotation as? UserAnnotation{
             let view = MKAnnotationView(annotation: annotation, reuseIdentifier: annotation.identifier)
             //change this to make the pin whatever image we want
-            view.image = avatarImage
+            if avatarImage != nil {
+                let resizeImage = self.resizeImage(image: avatarImage, targetSize: CGSize.init(width:60,height:60))
+                view.image = resizeImage
+            }
+            
+            //            view.image = avatarImage
             view.isEnabled = true
             view.canShowCallout = true
             //add image to callout:
