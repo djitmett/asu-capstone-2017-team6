ccbe78057c47930589d5d0bc498b5476e5b25283
Corey Rakes <crakes@asu.edu>
2018-02-27 22:25:41 -0500

    Added an array class to the UserAnnotation to store the user annoation objects

--- Ping!/Ping!/UserAnnotation.swift
+++ Ping!/Ping!/UserAnnotation.swift
@@ -19,5 +19,23 @@ class UserAnnotation: NSObject, MKAnnotation {
         self.coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
         self.image = avatarImage
     }
+}
+class UserAnnotationList: NSObject {
+    var userLocals = [UserAnnotation]()
+    override init() {
+        //
+    }
     
+    init(object: User) {
+        self.userLocals += [UserAnnotation(name: object.firstName, lat: object.latitude, long: object.longitude, avatarImage: object.avatar!)]
+    }
+    func addToList(object: User){
+        self.userLocals += [UserAnnotation(name: object.firstName, lat: object.latitude, long: object.longitude, avatarImage: object.avatar!)]
+    }
+    func getUserLocals() -> [UserAnnotation]{
+        return self.userLocals
+    }
+    func printList(){
+        print("UserLocals is count " ,userLocals.count)
+    }
 }
